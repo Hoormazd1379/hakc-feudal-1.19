9d08345e64be3a9885fef78ad5337d8ed58aeccf
Hoormazd <hoormazd.hp@gmail.com>
2022-11-11 21:51:23 +0100

    Initial commit

---
+++ src/main/java/net/hoormazd/hakcfeudal/HakcFeudalClient.java
@@ -0,0 +1,10 @@
+package net.hoormazd.hakcfeudal;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HakcFeudalClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
